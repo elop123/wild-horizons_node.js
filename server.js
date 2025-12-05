9ee5231fd3e681d15663c9b61523e08f20c78055
Elena <rlenka2010gmail.com>
2025-12-05 20:06:47 +0100

    adding utils map

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import http from 'node:http'
 import { getDataFromDB } from './db.js'
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
 
 const PORT = 8000
 
@@ -8,27 +9,22 @@ const destinations = await getDataFromDB()
 
 
 if(req.url === '/api' && req.method === 'GET'){
-
-res.setHeader('Content-Type', 'application/json')
-res.statusCode = 200
-res.end(JSON.stringify(destinations))
+sendJSONResponse(res,200, destinations)
 }else if(req.url.startsWith('/api/continent') && req.method === 'GET')
     {
-        const continent = req.url.split('/').pop()
-        //console.log(continent)
-        const filteredData =destinations.filter((destination)=>{
-        return destination.continent.toLowerCase() === continent.toLowerCase()
+    const continent = req.url.split('/').pop()
+    //console.log(continent)
+    const filteredData =destinations.filter((destination)=>{
+    return destination.continent.toLowerCase() === continent.toLowerCase()
         })
-res.setHeader('Content-Type', 'application/json')
-res.statusCode = 200
-res.end(JSON.stringify(filteredData))
+    sendJSONResponse(res,200, filteredData)
     } else{
     res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.end(JSON.stringify(
+    sendJSONResponse(res,404, (
         {error: "not found", 
-         message: "The requested route does not exist"
+        message: "The requested route does not exist"
     }))
+        
 }
 })
 
