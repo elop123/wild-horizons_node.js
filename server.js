267a5d634a22947c97b7d2ac54bc9e1cf3d3b510
Elena <rlenka2010gmail.com>
2025-12-05 19:28:47 +0100

    route not found

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ if(req.url === '/api' && req.method === 'GET'){
 res.setHeader('Content-Type', 'application/json')
 res.statusCode = 200
 res.end(JSON.stringify(destinations))
+}else{
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 404
+    res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"
+    }))
 }
 })
 
